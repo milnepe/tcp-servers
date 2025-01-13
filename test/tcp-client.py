a2c414d647e530c0dbc231391fed4d1527c8ec95
Peter Milne <milnepe@gmail.com>
2025-01-13 15:41:59 +0000

    Add a simple python test client

---
+++ test/tcp-client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Test TCP client which sends and receives a single message string then disconnects
+
+Run: tcp-test-client.py 127.0.0.1 65432
+"""
+
+import sys
+import socket
+
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <host> <port>")
+    sys.exit(1)
+
+host, port = sys.argv[1], int(sys.argv[2])
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Ensures the client socket gets closed if something goes wrong with send or recv
+    sock.settimeout(1)
+
+    sock.connect((host, port))
+    sock.sendall(b"Hello, world")
+    data = sock.recv(1024)
+
+print(f"Received {data!r}")
