263d517893780cb33d48589611a3646c9ad935f0
Peter Milne <milnepe@gmail.com>
2025-02-21 16:32:04 +0000

    Remove duplicate server

--- asyncio-server-command-addr-resp/python/test/get_mac_test.py
+++
@@ -1,9 +0,0 @@
-import uuid
- 
-# joins elements of getnode() after each 2 digits.
-def get_mac() -> str:
-    return ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
-    for ele in range(0,8*6,8)][::-1])
-
-print(get_mac())
-
